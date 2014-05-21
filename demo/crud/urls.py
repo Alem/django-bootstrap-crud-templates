3542514bf3a71f19ec416b926c02aa032efa717b
za <za@cidola.com>
2014-05-20 21:00:19 -0600

    Decrease items/page from default in demo app.

--- demo/crud/urls.py
+++ demo/crud/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from bsct.urls import URLGenerator
 from crud import models
 
-bsct_patterns = URLGenerator( models.Widget ).get_urlpatterns()
+bsct_patterns = URLGenerator( models.Widget ).get_urlpatterns( paginate_by = 3 )
 
 urlpatterns = patterns( '',
         url( '', include( bsct_patterns ) ) 
